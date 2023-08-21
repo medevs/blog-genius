7cca4e0a4092f2d935bf15c15fc45268e36d146f
Ahmed Oublihi <a.oublihi@ephilos.lokal>
2023-08-21 06:53:51 +0200

    outline improvement

--- app/api/outline/route.ts
+++ app/api/outline/route.ts
@@ -34,102 +34,64 @@ export async function POST(req: Request): Promise<Response> {
               {
                 title: "Introduction",
                 subheadings: [
-                  {
-                    heading: "Explanation of why blog writing is important",
-                  },
-                  {
-                    heading:
-                      "Brief overview of the key elements of a blog post",
-                  },
+                  { heading: "Explanation of why blog writing is important" },
+                  { heading: "Brief overview of the key elements of a blog post" },
                 ],
               },
               {
                 title: "Pre-Writing Steps",
                 subheadings: [
-                  {
-                    heading:
-                      "Define your target audience and purpose of the blog post",
-                  },
-                  {
-                    heading: "Conduct research and gather information",
-                  },
-                  {
-                    heading: "Create an outline to organize your thoughts",
-                  },
+                  { heading: "Define your target audience and purpose of the blog post" },
+                  { heading: "Conduct research and gather information" },
+                  { heading: "Create an outline to organize your thoughts" },
                 ],
               },
               {
                 title: "Writing the Blog Post",
                 subheadings: [
+                  { heading: "Craft an attention-grabbing headline" },
+                  { heading: "Write a compelling introduction" },
                   {
-                    heading: "Craft an attention-grabbing headline",
+                    heading: "Develop the main content with subheadings, bullet points, and examples"
                   },
-                  {
-                    heading: "Write a compelling introduction",
-                  },
-                  {
-                    heading:
-                      "Develop the main content with subheadings, bullet points, and examples",
-                  },
-                  {
-                    heading:
-                      "Use relevant images or multimedia to enhance the post",
-                  },
-                  {
-                    heading:
-                      "Edit and proofread for grammar, spelling, and coherence",
-                  },
+                  { heading: "Use relevant images or multimedia to enhance the post" },
+                  { heading: "Edit and proofread for grammar, spelling, and coherence" },
                 ],
               },
               {
                 title: "Formatting and Publishing",
                 subheadings: [
                   {
-                    heading:
-                      "Use appropriate formatting, such as short paragraphs and white space",
+                    heading: "Use appropriate formatting, such as short paragraphs and white space"
                   },
                   {
-                    heading:
-                      "Optimize for search engines with relevant keywords and meta tags",
+                    heading: "Optimize for search engines with relevant keywords and meta tags"
                   },
-                  {
-                    heading: "Add internal and external links to other content",
-                  },
-                  {
-                    heading: "Preview the post before publishing",
-                  },
+                  { heading: "Add internal and external links to other content" },
+                  { heading: "Preview the post before publishing" },
                 ],
               },
               {
                 title: "Promotion and Engagement",
                 subheadings: [
+                  { heading: "Share the post on social media and other channels" },
+                  { heading: "Respond to comments and feedback from readers" },
                   {
-                    heading:
-                      "Share the post on social media and other channels",
-                  },
-                  {
-                    heading: "Respond to comments and feedback from readers",
-                  },
-                  {
-                    heading:
-                      "Analyze and track performance metrics to improve future posts",
+                    heading: "Analyze and track performance metrics to improve future posts"
                   },
                 ],
               },
               {
                 title: "Conclusion",
                 subheadings: [
+                  { heading: "Recap of the key points for successful blog writing" },
                   {
-                    heading:
-                      "Recap of the key points for successful blog writing",
-                  },
-                  {
-                    heading:
-                      "Encouragement to continue improving and experimenting with blog writing",
+                    heading: "Encouragement to continue improving and experimenting with blog writing"
                   },
                 ],
               },
             ],
+            
           }),
           role: "assistant",
         },
